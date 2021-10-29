d5afffc45ebeb1fbb93e5d234f5d3a7564ed9bac
Netflixbr <93202081+Netflixbr@users.noreply.github.com>
2021-10-29 11:52:15 -0300

    Update index.php

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ Author: PARA (sp)
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
 	<link rel="stylesheet" type="text/css" href="css/estilo.css">
 	
-https://github.com/Netflixbr
+https://github.com/https://github.com/Netflixbr/netflixbr
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 	<script src="js/jquery-3.2.1.min.js"></script>
